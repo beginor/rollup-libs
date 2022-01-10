c41ced9decd4c018ced8e48e68b0625664459d87
beginor <beginor@qq.com>
2022-01-10 14:34:29 +0800

    rename

--- rollup/bootstrap.sh
+++ rollup/bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
-mkdir dist/libs/bootstrap
-cp -v node_modules/bootstrap/dist/css/bootstrap.min.css dist/libs/bootstrap
-cp -v node_modules/bootstrap/dist/js/bootstrap.esm.js dist/libs/bootstrap
-cp -v node_modules/bootstrap/dist/js/bootstrap.esm.min.js dist/libs/bootstrap
+target="dist/libs/bootstrap"
+if [ ! -d $target ]; then
+  mkdir $target
+fi
+cp -v node_modules/bootstrap/dist/css/bootstrap.min.css $target
+cp -v node_modules/bootstrap/dist/js/bootstrap.esm.js $target
+cp -v node_modules/bootstrap/dist/js/bootstrap.esm.min.js $target
