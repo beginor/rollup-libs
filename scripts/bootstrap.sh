69cdeb72f7443ac2d1725a14e87e0405ddeac2fc
beginor <beginor@qq.com>
2022-04-14 18:13:08 +0800

    refact bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,6 +3,6 @@ target="dist/libs/bootstrap"
 if [ ! -d $target ]; then
   mkdir $target
 fi
-cp -v node_modules/bootstrap/dist/css/bootstrap.min.css $target
+cp -v node_modules/bootstrap/dist/css/*.min.css $target
 cp -v node_modules/bootstrap/dist/js/bootstrap.esm.js $target
 cp -v node_modules/bootstrap/dist/js/bootstrap.esm.min.js $target
