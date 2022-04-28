f4112aa593d96bb11b8045b07b5a3e676cff341d
beginor <beginor@qq.com>
2022-04-28 18:32:22 +0800

    refact deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,30 @@
 #!/bin/bash -e
-rsync -avz dist/libs lighthouse:~/docker/nginx/html/dist
-rsync -avz importmap.* lighthouse:~/docker/nginx/html/dist
-rsync -avz loader.js lighthouse:~/docker/nginx/html/dist
+
+# default targets
+target="/Users/${USER}/Developer/containers/nginx/html/dist"
+target_url="http:\/\/localhost\/dist"
+
+original_url="http:\/\/localhost:3000\/dist"
+
+echo $1
+echo $2
+
+if [ ! -z $1 ]; then
+    target=$1
+fi
+
+if [ ! -z $2 ]; then
+    target_url=$2
+fi
+
+cp -f importmap.* dist
+
+sed -i.bak "s/${original_url}/${target_url}/g" dist/importmap.json
+sed -i.bak "s/${original_url}/${target_url}/g" dist/importmap.prod.json
+rm -f dist/*.bak
+rsync -rvz dist/importmap.* $target
+rm -f dist/importmap.*
+
+rsync -rvz dist/libs $target
+rsync -rvz loader.js $target
+
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,5 +4,5 @@ if [ ! -d $target ]; then
   mkdir $target
 fi
 cp -v node_modules/bootstrap/dist/css/*.min.css $target
-cp -v node_modules/bootstrap/dist/js/bootstrap.esm.js $target
-cp -v node_modules/bootstrap/dist/js/bootstrap.esm.min.js $target
+cp -v node_modules/bootstrap/dist/js/bootstrap.esm.js $target/bootstrap.js
+cp -v node_modules/bootstrap/dist/js/bootstrap.esm.min.js $target/bootstrap.min.js
