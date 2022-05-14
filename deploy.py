ff8a932e3d3fab15eb561df2925461de0620b69e
beginor <beginor@qq.com>
2022-05-14 08:21:00 +0800

    fix watch

--- deploy.py
+++ deploy.py
@@ -3,11 +3,11 @@ from os import system
 
 targets = {
     '~/Developer/containers/nginx/html/dist': 'http://localhost/dist',
-    # '~/Developer/python/geep/client/dist': 'http://172.32.32.206/geep/web/dist',
-    # 'lighthouse:~/docker/nginx/html/dist': 'http://81.71.93.249/dist',
-    # 'htaly:/mnt/nginx/html/dist': 'http://112.74.67.213:8080/dist',
-    # 'agsserver:/mnt/data/docker/nginx/html/arcgis-js-api/dist': 'https://it.gdeei.cn/arcgis-js-api/dist',
-    # 'agsportal:/mnt/data/docker/nginx/html/arcgis-js-api/dist': 'https://app.gdeei.cn/arcgis-js-api/dist',
+    '~/Developer/python/geep/client/dist': 'http://172.32.32.206/geep/web/dist',
+    'lighthouse:~/docker/nginx/html/dist': 'http://81.71.93.249/dist',
+    'htaly:/mnt/nginx/html/dist': 'http://112.74.67.213:8080/dist',
+    #'agsserver:/mnt/data/docker/nginx/html/arcgis-js-api/dist': 'https://it.gdeei.cn/arcgis-js-api/dist',
+    #'agsportal:/mnt/data/docker/nginx/html/arcgis-js-api/dist': 'https://app.gdeei.cn/arcgis-js-api/dist',
 }
 
 def copy_to(source: str, target: str):
