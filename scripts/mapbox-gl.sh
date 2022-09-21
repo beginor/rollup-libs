35bb8253c4252fec6969a3426cfb15b9001fba6e
beginor <beginor@qq.com>
2022-09-22 07:58:16 +0800

    add mapbox-gl/style-spec

--- scripts/mapbox-gl.sh
+++ scripts/mapbox-gl.sh
@@ -3,3 +3,4 @@ node scripts/mapbox-gl.esbuild.mjs
 cp -v node_modules/mapbox-gl/dist/mapbox-gl.css dist/libs/mapbox-gl
 cp -v node_modules/mapbox-gl/dist/mapbox-gl-unminified.js dist/libs/mapbox-gl/mapbox-gl-umd.js
 cp -v node_modules/mapbox-gl/dist/mapbox-gl.js dist/libs/mapbox-gl/mapbox-gl-umd.min.js
+cp -v node_modules/mapbox-gl/dist/style-spec/index.es.js dist/libs/mapbox-gl/style-spec.js
