e408f2ccd68c41e19b050cd8fd6e161c587ce37e
beginor <beginor@qq.com>
2023-02-28 09:05:18 +0800

    fix axios

--- scripts/axios.sh
+++ scripts/axios.sh
@@ -1,2 +1,8 @@
 #!/bin/bash -e
-node scripts/axios.esbuild.mjs
+target="dist/libs/axios"
+if [ ! -d $target ]; then
+  mkdir $target
+fi
+
+cp -v node_modules/axios/dist/esm/axios.js $target/axios.js
+cp -v node_modules/axios/dist/esm/axios.min.js $target/axios.min.js
