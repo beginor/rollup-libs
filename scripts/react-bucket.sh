cccfe65a48354542f63cd94ccfb117b31ee9a162
beginor <beginor@qq.com>
2023-03-19 11:09:35 +0800

    update react bucket scripts

--- scripts/react-bucket.sh
+++ scripts/react-bucket.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -e
-pnpm exec rollup -c scripts/react.rollup.mjs
+target="dist/libs/react-bucket"
+
+if [ -d $target ]; then
+  rm -rf $target
+fi
+
+pnpm exec rollup -c scripts/react-bucket.rollup.mjs
+
+cp -v node_modules/antd/dist/reset.css $target/antd.css
