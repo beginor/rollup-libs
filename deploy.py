7a00844d214bfb8d15ccd30ccd6268d8d6af968c
beginor <beginor@qq.com>
2023-04-30 11:08:24 +0800

    format

--- deploy.py
+++ deploy.py
@@ -2,7 +2,7 @@
 from os import system
 
 targets = {
-    '~/Developer/containers/nginx/app/html/dist': 'http://localhost/dist',
+    '~/Developer/containers/nginx/app/html/dist': 'http://127.0.0.1/dist',
     'lighthouse:~/docker/nginx/html/dist': 'http://81.71.93.249/dist',
     'ubuntu_zhang:~/Docker/nginx/html/dist': 'http://192.168.8.8/dist',
     # '~/Developer/python/geep/client/dist': 'http://172.32.32.206/geep/web/dist',
@@ -10,10 +10,12 @@ targets = {
     'agsdb01:/mnt/data/docker/nginx/app/html/arcgis-js-api/dist': 'https://app.gdeei.cn/arcgis-js-api/dist',
 }
 
+
 def copy_to(source: str, target: str):
     command = 'rsync -rzh --stats ' if target.find(':') > 0 else 'cp -rf '
     command = f'{command} {source} {target}'
     system(command)
+
 
 def deploy_to(target: str, target_url: str):
     print(f'deploy to {target} , target url is {target_url}')
@@ -30,6 +32,7 @@ def deploy_to(target: str, target_url: str):
     system('rm -f dist/importmap.*')
     copy_to('dist/libs', target)
     copy_to('dist/loader.*', target)
+
 
 for key in targets.keys():
     deploy_to(key, targets[key])
