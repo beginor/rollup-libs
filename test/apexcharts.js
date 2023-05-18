cb191d296b8726276856d624bee5a9cb4f608a25
beginor <beginor@qq.com>
2023-05-18 21:40:17 +0800

    appex charts test

--- test/apexcharts.js
+++ test/apexcharts.js
@@ -3,93 +3,35 @@ import ApexCharts from "apexcharts";
 const res = await fetch('/dist/libs/apexcharts/locales/zh-cn.json');
 const json = await res.json();
 
-
 /** @type {import("apexcharts").ApexOptions } */
-const options = {
+var options = {
+  series: [44, 55, 67, 83],
   chart: {
-    locales: [json],
-    defaultLocale: 'zh-cn',
-    type: "area",
-    fontFamily: 'inherit',
-    height: 480,
-    width: 960,
-    parentHeightOffset: 0,
-    toolbar: {
-      show: false,
-    },
-    animations: {
-      enabled: false
-    },
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  fill: {
-    opacity: .16,
-    type: 'solid'
-  },
-  stroke: {
-    width: 2,
-    lineCap: "round",
-    curve: "smooth",
-  },
-  series: [{
-    name: "series1",
-    data: [56, 40, 39, 47, 34, 48, 44]
-  },{
-    name: "series2",
-    data: [45, 43, 30, 23, 38, 39, 54]
-  }],
-  tooltip: {
-    theme: 'dark'
-  },
-  grid: {
-    padding: {
-      top: -20,
-      right: 0,
-      left: -4,
-      bottom: -4
-    },
-    strokeDashArray: 4,
-  },
-  xaxis: {
-    labels: {
-      padding: 0,
-    },
-    tooltip: {
-      enabled: false
-    },
-    axisBorder: {
-      show: false,
-    },
-    type: 'datetime',
-  },
-  yaxis: {
-    labels: {
-      padding: 4
-    },
-  },
-  labels: [
-    '2020-06-21', '2020-06-22', '2020-06-23', '2020-06-24', '2020-06-25', '2020-06-26', '2020-06-27'
-  ],
-  colors: [tabler.getColor("primary"), tabler.getColor("purple")],
-  legend: {
-    show: true,
-    position: 'bottom',
-    offsetY: 12,
-    markers: {
-      width: 10,
-      height: 10,
-      radius: 100,
-    },
-    itemMargin: {
-      horizontal: 8,
-      vertical: 8
-    },
-  },
+  height: 350,
+  type: 'radialBar',
+},
+plotOptions: {
+  radialBar: {
+    dataLabels: {
+      name: {
+        fontSize: '22px',
+      },
+      value: {
+        fontSize: '16px',
+      },
+      total: {
+        show: true,
+        label: 'Total',
+        formatter: function (w) {
+          // By default this function returns the average of all series. The below is just an example to show the use of custom formatter function
+          return 249
+        }
+      }
+    }
+  }
+},
+labels: ['Apples', 'Oranges', 'Bananas', 'Berries'],
 };
-const chart = new ApexCharts(
-  document.getElementById('charts'),
-  options
-);
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
 chart.render();
