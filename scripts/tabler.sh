7fa653a7b91a6d2c408d492ef19246b98b5c3b1b
beginor <beginor@qq.com>
2023-06-29 17:06:15 +0800

    use forcked tabler

--- scripts/tabler.sh
+++ scripts/tabler.sh
@@ -6,5 +6,6 @@ fi
 
 mkdir -p $target
 
-cp -r node_modules/@tabler/core/dist dist/libs/@tabler/core
+# cp -r node_modules/@tabler/core/dist dist/libs/@tabler/core
+cp -r ../tabler/dist dist/libs/@tabler/core
 cp -r node_modules/@tabler/icons dist/libs/@tabler/
