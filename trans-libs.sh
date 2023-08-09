c9e452a006a26d45795d3ce9248ff17235287597
beginor <beginor@qq.com>
2023-08-09 11:54:35 +0800

    add  cleanup and gzip command

--- trans-libs.sh
+++ trans-libs.sh
@@ -6,3 +6,10 @@ do
   echo executing $file
   $file
 done
+
+# cleanup
+rm -rf dist/chunks dist/main.*
+
+# Gzip static files (*.js, *.css).
+find dist/libs -type file -name "*.js" -print0 | xargs -0 gzip -k
+find dist/libs -type file -name "*.css" -print0 | xargs -0 gzip -k
