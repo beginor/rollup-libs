ea289c644b4e9f69bf1e787535bed20556046821
beginor <beginor@qq.com>
2023-10-20 14:08:49 +0800

    update test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 await loadStyles([
-  '/dist/libs/@tabler/core/css/tabler.min.css',
-  '/dist/libs/@tabler/core/css/tabler-vendors.min.css',
+  '../dist/libs/@tabler/core/css/tabler.min.css',
+  '../dist/libs/@tabler/core/css/tabler-vendors.min.css',
 ]);
 
 console.log('test loaded!');
